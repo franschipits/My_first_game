c680859cfb5df209b1d04219fffa3565510e9118
Franciane Schipits <franmifreitas@gmail.com>
2023-12-13 17:57:59 -0600

    game in progress

--- game.py
+++ game.py
@@ -6,8 +6,8 @@ screen = pygame.display.set_mode((800, 400)) #args (width, height)
 pygame.display.set_caption('My first game')
 clock = pygame.time.Clock() #creates a clock object
 
-test_surface = pygame.Surface((100, 200)) #args (width, height)
-test_surface.fill('Red') #fill surface with red color
+sky_surface = pygame.image.load('static/images/skycopy.png') #args (width, height)
+
 
 while True:
     for event in pygame.event.get(): #checking all events possible and looping through each event
@@ -17,8 +17,9 @@ while True:
     #draw all elements 
     #update everything
 
-    screen.blit(test_surface, (0,0)) #blit= block image transfer (put one surface in another surface) 
-                                     #2 arguments (surface, position)
+    screen.blit(sky_surface,(0,0)) #blit= block image transfer (put one surface in another surface) 
+                                         #2 arguments (surface, position)
+                                         #position = distance from left to right and from top to bottom
 
     pygame.display.update()
     clock.tick(60) #while loop shouldn't run faster than 60 times per second
